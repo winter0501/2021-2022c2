8c25a9f30ff851400d991b296f07c5b45dd2f5f6
winter0501 <91250436+winter0501@users.noreply.github.com>
2022-03-11 11:56:32 +0800

    Add files via upload

---
+++ week03/week03-4.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+char line[2000];
+int main()
+{
+	int t=1;
+    while(gets(line)){
+	///while(scanf("%s",line)==1){
+		if(t>1)printf("\n");
+		printf("車廂%d讀到一筆資料\n",t);
+		t++;
+	}
+	return 0;
+}
