daacc2d55c410caeeaa5387bb815a5a2487b8cf6
winter0501 <91250436+winter0501@users.noreply.github.com>
2022-05-20 09:42:36 +0800

    Update food.js

--- week12/food/food.js
+++ week12/food/food.js
@@ -15,9 +15,15 @@ function draw() {
         rect(50, 100, 150, 2);
     if (choice == 2)
         rect(50, 150, 150, 2);
+    if (choice == 3)
+        rect(50, 200, 150, 2);
+    if (choice == 4)
+        rect(50, 250, 150, 2);
     text("Choice 1", 50, 50);
     text("Choice 2", 50, 100);
     text("Choice 3", 50, 150);
+    text("Choice 4", 50, 200);
+    text("Choice 5", 50, 250);
 }
 
 function mousePressed() {
